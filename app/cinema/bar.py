c23d05dc5d20ceed4860252bbfb665d55b739e66
Andriy <andrijbuluk@gmail.com>
2025-12-19 10:09:45 +0100

    Test number one

---
+++ app/cinema/bar.py
@@ -0,0 +1,7 @@
+class CinemaBar:
+
+    @staticmethod
+    def sell_product(customer, product):
+        print(f" Покупець >>> {customer} <<< "
+              f" Продукт >>> {product} <<< ")
+
---
+++ app/cinema/hell.py
@@ -0,0 +1,20 @@
+from app.people.customer import Customer
+
+
+class CinemaHall:
+
+    def __init__(self, number_cinema):
+        self.number_cinema = number_cinema
+
+
+
+    def movie_session(self, movie_name, customer):
+        print(f" {movie_name} "
+              f" >>> {customer} ")
+
+
+
+
+    def cleaning_staff(self, Cleaner):
+        print(f" {watch_movie} ")
+
